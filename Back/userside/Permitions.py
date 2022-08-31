4ef761c1783633cfe28066731b88051a8badb88f
Abhijith <abhijithabhipgmr145@gmail.com>
2022-08-31 15:05:34 +0530

    all

--- Back/userside/Permitions.py
+++ Back/userside/Permitions.py
@@ -3,3 +3,5 @@ from rest_framework import permissions
 class UserPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.user.role == "admin" or(request.user.role == "manager" and (request.method=='GET' or request.method=='PUT')))
+        # return (request.user.usertype == "admin" or(request.user.usertype == "manager" and (request.method=='GET' or request.method=='PUT'))
+        # or (request.user.usertype == "staff" and (request.method == 'GET')))
